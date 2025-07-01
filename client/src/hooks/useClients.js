df79440b3e6d92fec008abb26d93ca6f9a06d161
yael <howitzer117@outlook.com>
2025-07-01 10:29:37 -0600

    correccion en el env de los hooks4

--- client/src/hooks/useClients.js
+++ client/src/hooks/useClients.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = process.env.VITE_APP_API_HOOKS || 'http://localhost:3000/NOVO';
+const API_BASE_URL = process.env.VITE_API_HOOKS || 'http://localhost:3000/NOVO';
 
 const useClients = () => {
     const [clients, setClients] = useState([]);
