9f7b13e01569ea1a4cd65d4a59759cb21e7feb9a
yael <howitzer117@outlook.com>
2025-07-01 10:15:36 -0600

    correccion en el env de los hooks2

--- client/src/hooks/useClients.js
+++ client/src/hooks/useClients.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const API_BASE_URL = process.env.VITE_APP_API_HOOKS || 'http://localhost:3000/NOVO';
-gir 
+
 const useClients = () => {
     const [clients, setClients] = useState([]);
     const [loadingClients, setLoadingClients] = useState(true);
